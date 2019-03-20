4fa805ef158d57f80fe60dde2955f73a31717eff
Narasimhan Madabusi <namadabu@microsoft.com>
2019-03-20 15:52:42 -0700

    Building out ExportJobRecord

---
+++ src/Microsoft.Health.Fhir.Core/Features/Export/ExportJobOutput.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using EnsureThat;
+
+namespace Microsoft.Health.Fhir.Core.Features.Export
+{
+    public class ExportJobOutput
+    {
+        public ExportJobOutput()
+            : this(new List<ExportJobOutputResult>(), new List<ExportJobOutputResult>())
+        {
+        }
+
+        public ExportJobOutput(List<ExportJobOutputResult> errors, List<ExportJobOutputResult> results)
+        {
+            EnsureArg.IsNotNull(errors);
+            EnsureArg.IsNotNull(results);
+
+            Errors = errors;
+            Results = results;
+        }
+
+        public List<ExportJobOutputResult> Errors { get; }
+
+        public List<ExportJobOutputResult> Results { get; }
+    }
+}
--- src/Microsoft.Health.Fhir.Core/Features/Export/ExportJobRecord.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Export/ExportJobRecord.cs
@@ -5,17 +5,21 @@
 
 using System;
 using EnsureThat;
+using Microsoft.Health.Fhir.Core.Messages.Export;
 using Newtonsoft.Json;
 
 namespace Microsoft.Health.Fhir.Core.Features.Export
 {
     public class ExportJobRecord
     {
+        private const string PartitionKey = "ExportJob";
+
         public ExportJobRecord(Uri requestUri)
         {
             EnsureArg.IsNotNull(requestUri, nameof(requestUri));
 
             RequestUri = requestUri;
+
             JobStatus = ExportJobStatus.Queued;
             Id = Guid.NewGuid().ToString();
         }
@@ -26,6 +30,22 @@ namespace Microsoft.Health.Fhir.Core.Features.Export
         public ExportJobStatus JobStatus { get; }
 
         public Uri RequestUri { get; }
+
+        public DateTimeOffset QueuedTimeStamp { get; }
+
+        public DateTimeOffset LastModified { get; }
+
+        public DateTimeOffset StartTimeStamp { get; }
+
+        public int NumberOfConsecutiveFailures { get; }
+
+        public int TotalNumberOfFailures { get; }
+
+        public ExportRequest Request { get; }
+
+        public ExportJobProgress Progress { get; }
+
+        public ExportJobOutput Output { get; }
 
         public void DummyMethod()
         {
